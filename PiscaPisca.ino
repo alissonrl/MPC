fe8a44d1aec62e013901fc06523f537ed9180e03
AlissonRL <alisson_rodolfo5@hotmail.com>
2020-10-22 17:36:23 -0300

    Adicionado outro ciclo

--- PiscaPisca.ino
+++ PiscaPisca.ino
@@ -39,4 +39,8 @@ void loop() {
   delay(500);                       // wait for a second
   digitalWrite(Pino, LOW);    // turn the LED off by making the voltage LOW
   delay(500);                       // wait for a second
+  digitalWrite(Pino, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1500);                       // wait for a second
+  digitalWrite(Pino, LOW);    // turn the LED off by making the voltage LOW
+  delay(1500);                       // wait for a second
 }
